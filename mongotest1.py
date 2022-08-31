dd5e843b325d31334b6d1235c0174746181e78c8
ARUP <arupkumar02@yahoo.com>
2022-08-31 23:35:45 +0530

    This is my first code to push in github

--- mongotest1.py
+++ mongotest1.py
@@ -3,10 +3,4 @@ client = pymongo.MongoClient("mongodb+srv://Swaroop:Arup1234@cluster0.esfsbtu.mo
 db = client.test
 print(db)
 
-client = pymongo.MongoClient("mongodb+srv://Swaroop:Arup1234@cluster0.esfsbtu.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
-client = pymongo.MongoClient("mongodb+srv://Swaroop:Arup1234@cluster0.esfsbtu.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
 
